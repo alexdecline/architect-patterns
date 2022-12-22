9e2412ef1dd876746f51fd76efd1611b4f804f96
e.kohan <e.kohan@smartdeal.pro>
2022-12-22 21:37:07 +0300

    add SpeedChangeable interface and execution logic

--- src/test/java/com/otus/spaceBattle/command/ChangeSpeedTest.java
+++ src/test/java/com/otus/spaceBattle/command/ChangeSpeedTest.java
@@ -1,6 +1,7 @@
 package com.otus.spaceBattle.command;
 
 import com.otus.spaceBattle.action.SpeedChangeable;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,15 +26,37 @@ public class ChangeSpeedTest {
 
     @Test
     void changeSpeed() {
+        //given
         Mockito.when(speedChangeable.getSpeed()).thenReturn(STARTED_SPEED);
         Mockito.when(speedChangeable.getSpeedChange()).thenReturn(CHANGED_SPEED);
 
+        //when
         speedChanger.execute();
 
+        //then
         Mockito.verify(speedChangeable).getSpeed();
         Mockito.verify(speedChangeable).getSpeedChange();
 
         Mockito.verify(speedChangeable).setSpeed(Mockito.eq(FINAL_SPEED));
+    }
+
+
+    @Test
+    void shouldThrowExceptionIfGetSpeedChangeNotAvailable() {
+        //given
+        Mockito.when(speedChangeable.getSpeedChange()).thenThrow(RuntimeException.class);
+
+        //when - then exception
+        Assertions.assertThrows(RuntimeException.class, () -> speedChanger.execute());
+    }
+
+    @Test
+    void executeShouldThrowExceptionIfGetSpeedNotAvailable() {
+        //given
+        Mockito.when(speedChangeable.getSpeed()).thenThrow(RuntimeException.class);
+
+        //when - then exception
+        Assertions.assertThrows(RuntimeException.class, () -> speedChanger.execute());
     }
 
 }
