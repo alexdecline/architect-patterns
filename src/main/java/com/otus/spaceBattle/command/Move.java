1dce8cc32956d6db71f2e7c5209e1d68b12e6b29
Alex Parfenov <alexdecline@gmail.com>
2023-01-19 21:33:33 +0300

    Move implement command =)

--- src/main/java/com/otus/spaceBattle/command/Move.java
+++ src/main/java/com/otus/spaceBattle/command/Move.java
@@ -3,7 +3,7 @@ package com.otus.spaceBattle.command;
 import com.otus.spaceBattle.action.Movable;
 import com.otus.spaceBattle.dto.Coords;
 
-public class Move {
+public class Move implements Command {
   private final Movable movable;
 
   public Move(Movable movable) {
